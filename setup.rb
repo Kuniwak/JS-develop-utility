1d41e2926393826d918f75bb318544743c0eb0df
OrgaChem <orga.chem.job@gmail.com>
2012-04-30 04:54:28 +0900

    FixBug: Create symbolic links if it is exists

--- setup.rb
+++ setup.rb
@@ -13,14 +13,14 @@ SCRIPT_DIR_PATH = File.dirname(File.expand_path(__FILE__))
 ROOT_DIR_PATH = "#{SCRIPT_DIR_PATH}/../"
 
 # Location of Readme files
-GITIGNORE_FILE_PATH = "#{SCRIPT_DIR_PATH}/_gitignore"
+GITIGNORE_FILE_PATH = "#{SCRIPT_DIR_PATH}/files/_gitignore"
 
 # Location of Readme symbolic links
 GITIGNORE_LINK_PATH = "#{ROOT_DIR_PATH}/.gitignore"
 
 # Location of Readme files
-README_FILE_PATH = "#{SCRIPT_DIR_PATH}/README.textile"
-README_JA_FILE_PATH = "#{SCRIPT_DIR_PATH}/README_Ja.textile"
+README_FILE_PATH = "#{SCRIPT_DIR_PATH}/files/README.textile"
+README_JA_FILE_PATH = "#{SCRIPT_DIR_PATH}/files/README_Ja.textile"
 
 # Location of Readme symbolic links
 README_LINK_PATH = "#{ROOT_DIR_PATH}/README.textile"
@@ -31,17 +31,20 @@ repo_name_ja = nil
 
 # Arguments processing
 opt = OptionParser.new()
-opt.on('-n', '--name', 'Repository name.') {|v| repo_name = v}
-opt.on('-j', '--name', 'Repository name in Japanese.') {|v| repo_name_ja = v}
+opt.on('-n <NAME>', '--name <NAME>', 'Repository name.') {|v| repo_name = v}
+opt.on('-j <NAME_Ja>', '--name_ja <NAME>', 'Repository name in Japanese.') {|v| repo_name_ja = v}
 argv = opt.parse!(ARGV)
 
-if !repo_name raise "Repository name is invalied: #{repo_name}"
-if !repo_name_ja raise "Repository name in Japanese is invalied: #{repo_name_ja}"
+if !repo_name
+	raise "Repository name is invalied: #{repo_name}"
+end
+if !repo_name_ja
+	raise "Repository name in Japanese is invalied: #{repo_name_ja}"
+end
 
-# Repository directory name is escaped " " to "-"
-repo_dir_name = repo_name.gsub(/ /, '-')
-
-def replace_parameters(path)
+def replace_parameters(path, repo_name, repo_name_ja)
+	# Repository directory name is escaped " " to "-"
+	repo_dir_name = repo_name.gsub(/ /, '-')
 	readme_tmp = []
 	File.open(path, 'r') do |readme|
 		readme.each do |line|
@@ -53,14 +56,21 @@ def replace_parameters(path)
 		end
 	end
 	File.open(path, 'w') do |readme|
-		readme.put(readme_tmp.join("")
+		readme.puts(readme_tmp.join(""))
 	end
 end
 
-replace_parameters(README_FILE_PATH)
-File.symlink(README_FILE_PATH, README_LINK_PATH)
+replace_parameters(README_FILE_PATH, repo_name, repo_name_ja)
+if !File.symlink?(README_LINK_PATH)
+	File.symlink(README_FILE_PATH, README_LINK_PATH)
+end
 
-replace_parameters(README_JA_FILE_PATH)
-File.symlink(README_JA_FILE_PATH, README_JA_LINK_PATH)
+replace_parameters(README_JA_FILE_PATH, repo_name, repo_name_ja)
+if !File.symlink?(README_JA_LINK_PATH)
+	File.symlink(README_JA_FILE_PATH, README_JA_LINK_PATH)
+end
 
-File.symlink(GITIGNORE_FILE_PATH, GITIGNORE_LINK_PATH)
+if !File.symlink?(GITIGNORE_LINK_PATH)
+	File.symlink(GITIGNORE_FILE_PATH, GITIGNORE_LINK_PATH)
+end
+
