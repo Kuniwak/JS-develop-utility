9472b61f3faa22fa5363897fc3a2c3e9e0d91db6
OrgaChem <orga.chem.job@gmail.com>
2012-04-30 17:37:40 +0900

    FixBug: -i argument require relational path but given absolute path

--- jsdoc.rb
+++ jsdoc.rb
@@ -18,7 +18,7 @@ ROOT_DIR_PATH = SCRIPT_DIR_PATH + "/.."
 # Absolute path of JsDoc utility directory
 JSDOC_UTIL_PATH = "#{SCRIPT_DIR_PATH}/jsdoc/jsdoc.rb"
 
-INPUT_DIR = ROOT_DIR_PATH
-OUTPUT_DIR = "#{ROOT_DIR_PATH}/docs"
+INPUT_DIR = '../'
+OUTPUT_DIR = '../docs'
 
 `ruby #{JSDOC_UTIL_PATH} -i #{INPUT_DIR} -o #{OUTPUT_DIR}`
